3a81af7d932b6d1b6a64c3d38063df89aa8de7a7
Cederic Nijssen <60871713+CedericN@users.noreply.github.com>
2021-04-21 21:01:23 +0200

    Update Workshop.py

--- Code/Workshop.py
+++ Code/Workshop.py
@@ -9,13 +9,14 @@ import Adafruit_DHT
 from subprocess import call 
 from picamera import PiCamera, Color
 
-#Variabele voor token en channel_ID
-TOKEN = 'PlaatsTokenHier'
-CHANNEL_ID = PlaatsChannel_IdHier
-
 #Toegang token voor discord server
+TOKEN = 'ODIzMjA4NDc4MjU5NTQ0MDg0.YFdeow.Z54zEz5Gv09vQXruzKEz7XdOtT8'
 description = '''IOT Workshop - Discord Bot'''
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='?', description=description, help_command=None)
+#bot = commands.Bot(command_prefix='?', description=description)
+pfp_path = "avatar.jpg"
+fp = open(pfp_path, 'rb')
+pfp = fp.read()
 
 #DHT declareren
 dht_sensor = Adafruit_DHT.DHT11
@@ -33,6 +34,7 @@ alarmActief = 0
 gpio2 = 21
 gesloten = 0
 GPIO.setup(gpio2,GPIO.OUT)
+GPIO.output(gpio2,GPIO.LOW)
 
 #CAMERA declaren + functie
 camera = PiCamera()
@@ -50,7 +52,9 @@ def convert_video(file_h264, file_mp4, lengteVideo):
 #Stuurt "motion detected" bij beweging
 async def motionDetection():
     await bot.wait_until_ready()
-    channel = bot.get_channel(CHANNEL_ID) 
+    channel = discord.utils.get(bot.get_all_channels(), name="algemeen")
+    #print(channel.id)
+    #channel = bot.get_channel(829712542741692457) # replace with channel ID that you want to send to
     msg_sent = False
 
     while True:
@@ -61,6 +65,7 @@ async def motionDetection():
                 if current_state == 1:
                     if not msg_sent:
                         await channel.send('Beweging gedetecteerd')
+                        print('beweging')
                         convert_video('/home/pi/Desktop/IOT_Workshop/video.h264', '/home/pi/Desktop/IOT_Workshop/video.mp4', 5)
                         await channel.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/video.mp4'))
                         commando = "rm " +"/home/pi/Desktop/IOT_Workshop/video.mp4"
@@ -76,29 +81,53 @@ async def motionDetection():
 
 @bot.event
 async def on_ready():
+    #await bot.user.edit(avatar = pfp)
     print('Bot opgestart en ingelogd als:')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
+@bot.group(invoke_witout_command=True)
+async def help(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(title = "Help", description = "Uitleg van comando's. gebruik ?help [commando] voor extra uitleg \n <> betekend moet gebruikt worden, [] betekend mag gebuikt worden", color = discord.Colour.blue())
+    embed.add_field(name="?purge <x>", value="verwijderen van x aanal berichten",  inline=False)
+    embed.add_field(name="?help", value="laat dit scherm",  inline=False)
+    embed.add_field(name="?foto", value="Er wordt foto gemaakt en doorgestuurd.",  inline=False)
+    embed.add_field(name="?video [x]", value="Er wordt een video van x seconden gemaakt en doorgestuurd\nAls er niks wordt megegeven wordt er een video van 3 seconden gemaakt",  inline=False)
+    embed.add_field(name="?beveiliging <x>", value="De beveiliging wordt aan of uit gezet \nGebruik Aan of Uit op de plaats van x",  inline=False)
+    embed.add_field(name="?meting", value="De temperatuur en luchtvochtigheid worden opgemeten.",  inline=False)
+    embed.add_field(name="?schakelaar <x>", value="De relais wordt aan en uit gezet.\nGebruik Aan of Uit op de plaats van x",  inline=False)
+
+    await ctx.send(embed = embed)
+
+@bot.command()
+async def purge(ctx, number):
+    """verwijderen van x aantal  berichten"""
+    await ctx.channel.purge(limit=1)
+    number = int(number) #aantal berichten om te verwijderen
+    await ctx.channel.purge(limit=number)
+
 @bot.command()
 async def foto(ctx):
     """Er wordt foto gemaakt en doorgestuurd."""
+    await ctx.channel.purge(limit=1)
     camera.capture('/home/pi/Desktop/IOT_Workshop/image.jpg')
     await ctx.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/image.jpg'))
     
 @bot.command()
-async def video(ctx):
-    """Er wordt een video van 3 seconden gemaakt en doorgestuurd."""
-    convert_video('/home/pi/Desktop/IOT_Workshop/video.h264', '/home/pi/Desktop/IOT_Workshop/video.mp4', 3)
+async def video(ctx,lenght : int=3):
+    """Er wordt een video van x seconden gemaakt en doorgestuurd.(als er niks wordt megegeven is het 3 seconden )"""
+    convert_video('/home/pi/Desktop/IOT_Workshop/video.h264', '/home/pi/Desktop/IOT_Workshop/video.mp4', lenght)
+    await ctx.channel.purge(limit=1)
     await ctx.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/video.mp4'))
     commando = "rm " +"/home/pi/Desktop/IOT_Workshop/video.mp4"
     call([commando], shell=True)
 
 @bot.command()
 async def beveiliging(ctx, alarm : str):
-    """Het alarm wordt aan en uit gezet (aan/uit)."""
-    alarm = alarm.lower()
+    """Het alarm wordt aan en uit gezet."""
+    await ctx.channel.purge(limit=1)
     global alarmActief
     if alarm == 'aan' and alarmActief == 0:
         alarmActief = 1
@@ -114,34 +143,93 @@ async def beveiliging(ctx, alarm : str):
         await ctx.send('Commando niet herkend: gebruik aan/uit')
 
 @bot.command()
+async def spam(ctx, number: int = 100):
+    """"voor bart."""
+    await ctx.channel.purge(limit=1)
+    for count in range(number):
+        await ctx.send('spammen')
+        time.sleep(0.5)
+        print(count)
+    time.sleep(5)
+    await ctx.channel.purge(limit=number)
+
+@bot.command()
 async def meting(ctx):
     """De temperatuur en luchtvochtigheid worden opgemeten."""
-    await ctx.send('De temperatuur en luchtvochtigheid worden gemeten, even geduld. De resultaten worden zodadelijk verstuurd.')
+    await ctx.channel.purge(limit=1)
+    await ctx.send('De temperatuur en luchtvochtigheid worden gemeten, even geduld. De resultaten worden zodadelijk verstuurd')
     humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio)
+    await ctx.channel.purge(limit=1)
     if humidity is not None and temperature is not None:
-        await ctx.send('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        await ctx.send('Temp={0:0.1f}°C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         await ctx.send('Ai, er ging iets mis bij het uitvoeren van de meting. Probeer het even opnieuw.')
 
 @bot.command()
 async def schakelaar(ctx, switch : str):
-    """De relais wordt aan en uit gezet (gesloten/open)."""
-    switch = switch.lower()
+    """De relais wordt aan en uit gezet."""
+    await ctx.channel.purge(limit=1)
     global gesloten
-    if switch == 'gesloten' and gesloten == 0:
+    if switch == 'aan' and gesloten == 0:
         GPIO.output(gpio2,GPIO.HIGH)
         gesloten = 1
-        await ctx.send('RELAIS Gesloten')
-    elif switch == 'open' and gesloten == 1:
+        await ctx.send("RELAIS Gesloten")
+    elif switch == 'uit' and gesloten == 1:
         GPIO.output(gpio2,GPIO.LOW)
         gesloten = 0
         await ctx.send("RELAIS Open")
-    elif switch == 'gesloten' and gesloten == 1:
-        await ctx.send('RELAIS Is Al Gesloten!')
-    elif switch == 'open' and gesloten == 0:
+    elif switch == 'aan' and gesloten == 1:
+        await ctx.send("RELAIS Is Al Gesloten!")
+    elif switch == 'uit' and gesloten == 0:
         await ctx.send("RELAIS Is Al Open!")
     else:
-        await ctx.send('Commando niet herkend: gebruik gesloten/open')
+        await ctx.send('Commando niet herkend: gebruik aan/uit')
 
+@bot.command()
+async def test(ctx):
+    await ctx.channel.purge(limit=1)
+    await ctx.send("test wordt gestart")
+    time.sleep(1)
+    await ctx.send("test RELAIS")
+    global gesloten
+    if gesloten == 1:
+        GPIO.output(gpio2,GPIO.LOW)
+        await ctx.send("RELAIS Open")
+        time.sleep(5)
+        GPIO.output(gpio2,GPIO.HIGH)
+        await ctx.send("RELAIS Gesloten")
+    else:
+        GPIO.output(gpio2,GPIO.HIGH)
+        await ctx.send("RELAIS Gesloten")
+        time.sleep(5)
+        GPIO.output(gpio2,GPIO.LOW)
+        await ctx.send("RELAIS Open")
+    time.sleep(1)
+    await ctx.send("test camera foto")
+    camera.capture('/home/pi/Desktop/IOT_Workshop/image.jpg')
+    await ctx.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/image.jpg'))
+    time.sleep(1)
+    await ctx.send("test camera Video")
+    convert_video('/home/pi/Desktop/IOT_Workshop/video.h264', '/home/pi/Desktop/IOT_Workshop/video.mp4', 3)
+    await ctx.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/video.mp4'))
+    commando = "rm " +"/home/pi/Desktop/IOT_Workshop/video.mp4"
+    call([commando], shell=True)
+    time.sleep(1)
+    await ctx.send("test DHT Sensor")
+    await ctx.send('De temperatuur en luchtvochtigheid worden gemeten, even geduld. De resultaten worden zodadelijk verstuurd')
+    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio)
+    await ctx.channel.purge(limit=1)
+    if humidity is not None and temperature is not None:
+        await ctx.send('Temp={0:0.1f}°C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        await ctx.send('Ai, er ging iets mis bij het uitvoeren van de meting. Probeer het even opnieuw.')
+    time.sleep(1)
+    await ctx.send("test motion detector")
+    current_state = 0
+    while current_state == 0:
+        current_state = GPIO.input(pir_sensor)
+        time.sleep(1)
+    await ctx.send("motion detected")
+    await ctx.send("test compleet")
 bot.loop.create_task(motionDetection())
 bot.run(TOKEN)
